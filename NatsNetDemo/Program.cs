0545e736002979932036fd121eef27a97f10e047
glorious73 <amjadmh73@gmail.com>
2024-05-18 17:10:56 +0300

    Initial commit

---
+++ NatsNetDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using NATS.Client.Core;
+
+await using var nats = new NatsConnection();
+
+var cts = new CancellationTokenSource();
+
+var subscription = Task.Run(async () =>
+{
+    await foreach (var msg in nats.SubscribeAsync<string>(subject: "foo").WithCancellation(cts.Token))
+    {
+        Console.WriteLine($"Received: {msg.Data}");
+    }
+});
+
+// Give subscription time to start
+await Task.Delay(1000);
+
+for (var i = 0; i < 10; i++)
+{
+    await nats.PublishAsync(subject: "foo", data: $"Hello, World! {i}");
+}
+
+// Give subscription time to receive messages
+await Task.Delay(1000);
+
+// Unsubscribe
+cts.Cancel();
+
+await subscription;
